ed4b16589afd15411762d11d0ff78654f2d1f599
kez <hide.kezu@gmail.com>
2022-01-30 23:18:21 +0900

    マイグレファイル追加漏れ

---
+++ www/myfuel/fuel/app/config/development/migrations.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'version'     => [
+        'app'     => [
+            'default' => [
+                0 => '001_create_posts',
+                1 => '002_create_articles',
+            ],
+        ],
+        'module'  => [],
+        'package' => [],
+    ],
+    'folder'      => 'migrations/',
+    'table'       => 'migration',
+    'flush_cache' => false,
+];
